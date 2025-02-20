60015403e326079ee38ee9ceade3f004c2863614
priyarajanharshini <priyamaha0707@gmail.com>
2025-02-20 10:50:55 +0530

    Tailwind Installation

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Sidebar from "./Component/Sidebar"
+
+function App() {
+  return (
+      <div>
+    {/* <h1 className="text-3xl font-bold bg-pink-500 text-green-400 text-center">
+      Hello world!
+    </h1> */}
+    <Sidebar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Sidebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+
+    <h1 className="container mx-auto bg-pink-100 text-pink-800 text-center text-3xl font-bold">
+      Sidebar
+    </h1>
+  );
+}
+
+export default App;
