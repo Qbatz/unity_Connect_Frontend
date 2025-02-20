c042402ba47728884d9af8cfd8d014ff7ea85578
Qbatz <qbatz.tech@gmail.com>
2025-02-20 12:41:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Sidebar from "./Component/Sidebar"
 
 function App() {
   return (
-      <div>
+      <div data-testid="container">
     {/* <h1 className="text-3xl font-bold bg-pink-500 text-green-400 text-center">
       Hello world!
     </h1> */}
