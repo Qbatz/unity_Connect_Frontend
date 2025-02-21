85cddb1c6dfb02d71b225475adf9b53ac9a5e645
priyarajanharshini <priyamaha0707@gmail.com>
2025-02-21 12:29:23 +0530

    ReduxSetup

---
+++ src/Redux/SagaMiddleware/RootSaga.js
@@ -0,0 +1,12 @@
+import {all} from 'redux-saga/effects';
+import SignInSaga from '../SagaMiddleware/SignInSaga';
+
+
+function* RootSaga() {
+    
+yield all([
+    SignInSaga(),
+
+])
+}
+export default RootSaga;
