426a91b9ed3ab22cf5297e36e168788ec7eb71b7
Mathubala <premathujasvi5706@gmail.com>
2025-02-21 12:17:30 +0530

    crypto code moved

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Sidebar from "./Component/Sidebar"
+import Crypto from './Crypto/crypto';
 
 function App() {
   return (
       <div data-testid="container">
     
     <Sidebar />
+    <Crypto />
     </div>
   );
 }
