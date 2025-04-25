15a31517d9fa9da617d4a97b11db78e5e8ec0f03
kavitha9894 <skavitha9894@gmail.com>
2025-04-25 17:21:05 +0530

    login issue

--- src/Redux/SagaMiddleware/SettingSaga.js
+++ src/Redux/SagaMiddleware/SettingSaga.js
@@ -151,54 +151,52 @@ export function* SettingTransactionID(action) {
 }
 
 function* SettingAddLoanPage(action) {
-    try {
-        const response = yield call(SettingAddLoan, action.payload);
 
-        var toastStyle = {
-            backgroundColor: "#E6F6E6",
-            color: "black",
-            width: "300px",
-            borderRadius: "60px",
-            height: "20px",
-            fontFamily: "Gilroy",
-            fontWeight: 600,
-            fontSize: 14,
-            textAlign: "start",
-            display: "flex",
-            alignItems: "center",
-            padding: "13px",
-        };
+    const response = yield call(SettingAddLoan, action.payload);
 
-        if (response?.status === 200 || response?.statusCode === 200) {
-            yield put({
-                type: "SETTINGADDLOAN",
-                payload: {
-                    loan_name: response.data.loan_name,
-                    due_on: response.data.due_on,
-                    due_type: response.data.due_type,
-                    due_count: response.data.due_count,
-                    statusCode: response.status || response.statusCode,
-                },
-            });
+    var toastStyle = {
+        backgroundColor: "#E6F6E6",
+        color: "black",
+        width: "300px",
+        borderRadius: "60px",
+        height: "20px",
+        fontFamily: "Gilroy",
+        fontWeight: 600,
+        fontSize: 14,
+        textAlign: "start",
+        display: "flex",
+        alignItems: "center",
+        padding: "13px",
+    };
 
-            toast.success(response.data.message, {
-                position: "bottom-center",
-                autoClose: 2000,
-                hideProgressBar: true,
-                closeButton: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                style: toastStyle,
-            });
-        } else {
-            toast.error("Failed to add loan");
-        }
-    } catch (error) {
-        console.error("Saga API Error:", error);
-        toast.error("Failed to add loan");
+    if (response?.status === 200 || response?.statusCode === 200) {
+        yield put({
+            type: "SETTINGADDLOAN",
+            payload: {
+                loan_name: response.data.loan_name,
+                due_on: response.data.due_on,
+                due_type: response.data.due_type,
+                due_count: response.data.due_count,
+                statusCode: response.status || response.statusCode,
+            },
+        });
+
+        toast.success(response.data.message, {
+            position: "bottom-center",
+            autoClose: 2000,
+            hideProgressBar: true,
+            closeButton: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            style: toastStyle,
+        });
     }
+    if (response) {
+        refreshToken(response);
+    }
+
 }
 
 
